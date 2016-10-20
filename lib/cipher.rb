8a928289a6e794fdbc4b1b130742760b1c5bc77c
Josef Perry Tinagan <josephperry_06@yahoo.com>
2016-10-20 19:32:18 +0800

    shifting, retaining cases, retaining spaces

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,11 +1,63 @@
 class Cipher
-
-
-	def say_hello
-		"Hello"
-	end
+	
 
 	def caesar_cipher(word,shift)
-		word + shift.to_s
+		arr = ["a","b","c","d","e","f","g","h","i","j","k","l",
+				"m","n","o","p","q","r","s","t","u","v","w",
+				"x","y","z"]
+		
+		word = word.split("")
+		x = 0
+		result_string = ""
+
+
+
+		while word.length > x 
+			y = 0
+			capital = false
+			has_space = false
+
+			if(word[x] == " ")
+				has_space = true
+			end
+
+			if(has_space)
+
+				result_string += word[x]
+				x += 1
+
+			else
+
+				if(word[x] == word[x].upcase)
+					word[x].downcase!
+					capital = true
+				end
+
+					while word[x] != arr[y]
+
+						y += 1
+					end
+
+				temp = arr[y+shift]
+
+				if capital
+					temp = temp.upcase
+				else
+					temp = temp
+				end
+
+				result_string += temp
+
+				x += 1
+
+			end
+				
+		end
+
+		return result_string
+
 	end
+
+	
+
 end
--- spec/lib/cipher_spec.rb
+++ spec/lib/cipher_spec.rb
@@ -4,15 +4,46 @@ require "cipher"
 
 describe Cipher do
 
-	it "The say_hello method should return 'Hello'" do
+#	it "The say_hello method should return 'Hello'" do
+#		c = Cipher.new
+#   	expect(c.say_hello).to eq "Hello"
+
+#	end
+
+	it "spliting words correctly" do 
 		c = Cipher.new
-		expect(c.say_hello).to eq "Hello"
+		param = c.caesar_cipher("abc",2)
+		expect(param).to eq "cde"
 
 	end
 
-	it "The caesar_cipher method should accept parameters" do
+	it "retaining capitalization" do
 		c = Cipher.new
-		param = c.caesar_cipher("test",1)
-		expect(param).to eq "test1"
+		param = c.caesar_cipher("AaA",2)
+		expect(param).to eq "CcC"
+
 	end
+
+	it "capitalization examples drool" do
+		c = Cipher.new
+		param = c.caesar_cipher("Drool",3)
+		expect(param).to eq "Gurro"
+
+	end
+
+	it "spacing matters" do
+		c = Cipher.new
+		param = c.caesar_cipher("Ben Ten",1)
+		expect(param).to eq "Cfo Ufo"
+
+	end
+
+	xit "can go back to start from z to a" do
+		c = Cipher.new
+		param = c.caesar_cipher("z",1)
+		expect(param).to eq "a"
+	end
+
+
+
 end
